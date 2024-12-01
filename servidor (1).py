3c4de21d204481f1d2d6463ece55365c43a0d600
978sebasBeller <sebastian.beller@ucb.edu.bo>
2024-12-01 19:48:57 -0400

    Face Recognition with face net

--- servidor (1).py
+++ servidor (1).py
@@ -5,12 +5,48 @@ import numpy as np
 import base64
 from tensorflow import keras
 from keras.models import load_model
-labels=['Ale', 'Noni', 'Sebas']
+from keras_facenet import FaceNet
+labels=['Agus','Ale', 'Noni', 'Sebas']
 app = Flask(__name__)
 CORS(app) 
+
 model=None
+
+embedder = FaceNet()
+# Función para generar el embedding usando FaceNet
+def calculate_face_net(images):
+    images=images.astype('float32')
+    images = np.expand_dims(images, axis=0)
+    embeddings = embedder.embeddings(images)
+    #print(embeddings[0])
+    return embeddings[0] 
+
+def preprocess_images(img):
+    """
+    Preprocesa una lista de imágenes: las convierte a RGB, redimensiona y normaliza
+    """
+    img=cv2.cvtColor(cv2.resize(img, (160, 160)), cv2.COLOR_BGR2RGB)
+    img=np.array(img)
+
+    #print(np.array(processed_images))
+    return img  # Devuelve un lote de imágenes
+
+
+def normalize_matrix(matrix):
+    normalized_matrix = np.zeros_like(matrix)  # Crear matriz del mismo tamaño
+    for i in range(matrix.shape[0]):  # Iterar por cada fila (embedding)
+        X_min = np.min(matrix[i])  # Mínimo de la fila
+        X_max = np.max(matrix[i])  # Máximo de la fila
+        if X_max != X_min:  # Evitar división por cero
+            normalized_matrix[i] = (matrix[i] - X_min) / (X_max - X_min)
+        else:
+            normalized_matrix[i] = matrix[i]  # Si min == max, no se normaliza
+    return normalized_matrix
+
+
+
 def get_model():
-    return load_model("./CNNrostros.h5") if model is None else model
+    return load_model("./modelo14.h5") if model is None else model
 @app.route('/upload', methods=['POST'])
 def upload():
     datos = request.get_json()
@@ -40,13 +76,23 @@ def upload():
                   rostro=imagenCopia[y+reducciony:y+h-reducciony,x+reduccionx:x+w-reduccionx]
                   if rostro.size>0:
                     rostroReducido= cv2.resize(rostro, (200, 200))
-                    rostroEscalaDeGrises=cv2.cvtColor(rostroReducido,cv2.COLOR_BGR2GRAY)/255.0
-                    rostroEscalaDeGrises=rostroEscalaDeGrises.flatten().T.reshape(1, -1)
-                    print(rostroEscalaDeGrises.shape)
-                    porcentajes=model.predict(rostroEscalaDeGrises)
+                    print(np.array(rostroReducido).shape)#200x200x3
+                    rostroReducidoProcessed=preprocess_images(rostroReducido)
+                    print(rostroReducidoProcessed.shape)
+                    embedding=calculate_face_net(rostroReducidoProcessed)
+                    print("Embedding",embedding.shape)
+                    embedding = np.expand_dims(embedding, axis=0)  # Ahora tendrá forma (1, 512)
+                    embedding = normalize_matrix(embedding)
+
+                    print("Embedding2",embedding.shape)
+
+                    porcentajes=model.predict(embedding)
                     persona=labels[np.argmax(porcentajes)] if np.max(porcentajes)>0.50 else "Desconocido" 
-    print(imagen_gris.shape)
-    _, buffer = cv2.imencode('.jpg', imagenCopia)
+                    cv2.putText(imagen,f"{persona}"
+                                ,(x,y-50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0),2)
+                    cv2.putText(imagen,f"{np.max(porcentajes):.2f}"
+                                ,(x,y-5), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0),2)
+    _, buffer = cv2.imencode('.jpg', imagen)
     imagen_gris_base64 = base64.b64encode(buffer).decode('utf-8')
 
     return jsonify({'imagen_procesada': imagen_gris_base64,'label':persona}), 200
